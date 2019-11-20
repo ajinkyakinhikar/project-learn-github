8ed425a7a5ad1e4682c2cf8b39ce9327cc2b8d15
Ajinkya <ajinkyakinhikar@gmail.com>
2019-11-20 14:00:28 +0530

    code for addition of two numbers in hello_world.py file

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,12 @@
 print("Hello World!")
 
 # Now it's your turn. Write some code.. 
+# I am adding code..
+
+def add(a,d):
+    return a + d
+
+n1 = float(input("Enter a number: /n>"))
+n2 = float(input("Enter another for addition: /n>"))
+
+print(f"The addition of {n1} and {n2} is {add(n1, n2)}.")
